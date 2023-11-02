a85305e63f919646ce642f8cc5f83ee85d4ea8ec
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-11-02 22:18:08 +0500

    Task2

---
+++ Tyuiu.PozhdinAA.Sprint4.Task2.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PozhdinAA.Sprint4.Task2.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PozhdinAA.Sprint4.Task2.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1276b842-7eed-414a-9be9-89dc88a3b221")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PozhdinAA.Sprint4.Task2.V7/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PozhdinAA.Sprint4.Task2.V7.Lib;
+
+namespace Tyuiu.PozhdinAA.Sprint4.Task2.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Пождин А.А. | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Пождин Александр Андреевич  | АСОиУБ-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 12 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 4 до 9 подсчитать произведение четных         *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int len;
+            Console.WriteLine("Введите количество элементов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[len];
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                array[i] = rnd.Next(4, 9);
+            }
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Результат произведения чётных элементов: " + ds.Calculate(array));
+            Console.ReadKey();
+        }
+    }
+}
