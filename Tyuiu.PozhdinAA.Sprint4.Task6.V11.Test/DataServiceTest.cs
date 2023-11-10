b263b6027a05943937d7df6e4081acd7811eedd8
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-11-11 00:15:42 +0500

    Task6

---
+++ Tyuiu.PozhdinAA.Sprint4.Task6.V11.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PozhdinAA.Sprint4.Task6.V11.Lib;
+
+namespace Tyuiu.PozhdinAA.Sprint4.Task6.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int wait = 3;
+            string[] array = new string[] { "Кошка", "Собака", "Слон", "Жираф", "Бегемот", "Игуана", "Ягуар" };
+            int res = ds.Calculate(array);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PozhdinAA.Sprint4.Task6.V11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PozhdinAA.Sprint4.Task6.V11.Lib;
+
+namespace Tyuiu.PozhdinAA.Sprint4.Task6.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил Пождин А.А. | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание №6                                                              *");
+            Console.WriteLine("* Вариант №11                                                             *");
+            Console.WriteLine("* Выполнил: Пождин Александр Андреевич | АСОиУБ-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Подсчитайте количество элементов, длина которых равна 5                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*       { Кошка, Собака, Слон, Жираф, Бегемот, Игуана, Ягуар }            *");
+            DataService ds = new DataService();
+            string[] array = new string[] { "Кошка", "Собака", "Слон", "Жираф", "Бегемот", "Игуана", "Ягуар" };
+            int res = ds.Calculate(array);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
